a0cfec51d22618b78424dc12646997b6a7521a51
StevenWuTG <stevenwu91@icloud.com>
2021-07-17 16:17:38 -0400

    object typing

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
-var printResult = true;
-var resultPhrase = 'Total is: ';
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult === true) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-var number1 = 5;
-var number2 = 2.8;
-add(number1, number2, printResult, resultPhrase);
+var steven = {
+    name: 'steven',
+    age: 29
+};
+console.log(steven.age);
--- app.ts
+++ app.ts
@@ -1,20 +1,9 @@
-const printResult = true
-const resultPhrase = 'Total is: '
-
-function add( n1: number, n2: number, showResult: boolean, phrase: string) {
-
-    const result = n1 + n2
-
-    if(showResult === true){
-        console.log(phrase + result)
-    } else {
-        return result
-    }
-
-
+const steven: {
+    name:string;
+    age:number;
+} = {
+    name: 'steven',
+    age:29
 }
 
-const number1 = 5
-const number2 = 2.8
-
-add(number1, number2, printResult, resultPhrase)
+console.log( steven.age )
