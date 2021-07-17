005dea14cd5da71d0dd918b7d3617db94f1c1f0c
StevenWuTG <stevenwu91@icloud.com>
2021-07-17 16:25:42 -0400

    type inference

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-var steven = {
+var steven 
+// : {
+//     name:string;
+//     age:number;
+// } 
+= {
     name: 'steven',
-    age: 29
+    age: 29,
+    hobbies: ['cooking', 'gaming']
 };
-console.log(steven.age);
+var favHobbies;
+favHobbies = ['gaming'];
+console.log(steven.name);
+for (var _i = 0, _a = steven.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- app.ts
+++ app.ts
@@ -1,9 +1,20 @@
-const steven: {
-    name:string;
-    age:number;
-} = {
+const steven
+// : {
+//     name:string;
+//     age:number;
+// } 
+= {
     name: 'steven',
-    age:29
+    age:29,
+    hobbies: ['cooking','gaming'],
+
 }
 
-console.log( steven.age )
+let favHobbies: string[]
+favHobbies = ['gaming']
+
+console.log( steven.name )
+
+for (const hobby of steven.hobbies){
+    console.log(hobby.toUpperCase())
+}
